4125303820c75de74e51742488cd3b551dc85cae
Prithvi824 <Prithvisr003@gmail.com>
2024-07-04 10:55:04 +0530

    Bug fixes

--- Bot/server/main.py
+++ Bot/server/main.py
@@ -30,7 +30,7 @@ def download_reel(reel):
 
     # Create Url, payload and headers
     url = "https://v3.saveig.app/api/ajaxSearch"
-    payload = f"q={urllib.parse.quote(reel, safe="")}&t=media&lang=en"
+    payload = f"q={urllib.parse.quote(reel, safe='')}&t=media&lang=en"
     headers = {
     'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
